760ab50969be12f55bd12fa169269e75e4798e70
Frank Mathy <frankmathy@gmail.com>
2020-09-27 17:11:52 +0200

    Loading list of countries

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from "react";
-import { MenuItem, FormControl, Select } from "@material-ui/core";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { MenuItem, FormControl, Select } from '@material-ui/core';
+import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID 19 tracker</h1>
         <FormControl className="app_dropdown">
           <Select variant="outlined" value="abc">
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="option2">Option 2</MenuItem>
-            <MenuItem value="option3">Option 3</MenuItem>
-            <MenuItem value="option4">Option 4</MenuItem>
+            {countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
